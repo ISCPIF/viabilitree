c16bd4ebbefd3021aa41615f8f207c9993036e8b
Ricardo De Aldama <ricardo.de.aldama@gmail.com>
2013-02-26 17:12:08 +0100

    Add root project build configuration

---
+++ project/BuildViabilityRoot.scala
@@ -0,0 +1,17 @@
+import sbt._
+import Keys._
+
+object ViabilityRootBuild extends Build { 
+
+ lazy val viabilityRoot = Project(id = "viabilityRoot", base = file(".")) aggregate(viabilityLanguages, viabilityCabbage) dependsOn(viabilityLanguages, viabilityCabbage, kdtree)
+
+ lazy val kdtree = Project(id = "kdtree", base = file("kdtree")) 
+ 
+ lazy val languages = Project(id = "languages", base = file("languages"))  
+
+ lazy val viabilityLanguages = Project(id = "viabilityLanguages", base = file("viabilityLanguages")) aggregate(languages, kdtree) dependsOn(languages, kdtree)
+
+ lazy val viabilityCabbage = Project(id = "viabilityCabbage", base = file("viabilityCabbage")) dependsOn(kdtree) 
+
+}
+
