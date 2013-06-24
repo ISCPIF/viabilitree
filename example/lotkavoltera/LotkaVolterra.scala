7f9bf832b1485d52e2053cf2d6de9d3042c01472
Romain Reuillon <romain.reuillon@iscpif.fr>
2013-06-24 16:45:04 +0200

    Refactor model.

--- example/lotkavoltera/LotkaVolterra.scala
+++ example/lotkavoltera/LotkaVolterra.scala
@@ -2,29 +2,22 @@ package lotkavoltera
 
 object LotkaVolterra extends App {
 
-  val alpha = 0.07
-  val beta = 0.008
-  val gama = 0.2
-  val teta = 0.01
+  val alpha = 2.0
+  val beta = 0.4
+  val gamma = 1.0
+  val delta = 0.1
 
   val timeStep = 0.001
 
   def xDot(state: Array[Double], t: Double) = state(0) * (alpha - beta * state(1))
-  def yDot(state: Array[Double], t: Double) = -state(1) * (gama - teta * state(0))
+  def yDot(state: Array[Double], t: Double) = -state(1) * (gamma - delta * state(0))
 
   val dynamic = Dynamic(xDot, yDot)
 
-  def apply(x: Double, y: Double, time: Double) = dynamic.integrate(Array(x, y), timeStep, Seq(0.0, time)).last._2.toSeq
-
-  for {
-    x <- 0.0 to 40 by 0.5
-    y <- 0.0 to 40 by 0.5
-  } {
-    val res = apply(x, y, 0.05)
-    println(s"set arrow from $x,$y to ${res(0)},${res(1)}")
+  def apply(x: Double, y: Double, time: Double) = {
+    val res = dynamic.integrate(Array(x, y), timeStep, Seq(0.0, time)).last._2.toSeq
+    (res(0), res(1))
   }
-
-  println("plot [0:100] [0:100] 0")
 
 }
 
