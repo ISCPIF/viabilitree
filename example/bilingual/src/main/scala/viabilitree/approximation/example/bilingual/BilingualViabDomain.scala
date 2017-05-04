22463302fe56d680cebeb7ea9dda50a982d60ca0
ia <isabelle.alvarez@lip6.fr>
2017-05-04 17:19:10 +0200

    nilingual kernel

--- example/bilingual/src/main/scala/viabilitree/approximation/example/bilingual/BilingualViabDomain.scala
+++ example/bilingual/src/main/scala/viabilitree/approximation/example/bilingual/BilingualViabDomain.scala
@@ -17,14 +17,15 @@ object BilingualViabDomain extends App {
     depth = 18,
     zone = Vector((0.2, 1.0), (0.2, 1.0), (0.0, 1.0)),
     controls = Vector((0.1 to -0.1 by -0.01)),
+    k = Some(p => p(0) <= 1 && p(0) >= 0.2 && p(1) <= 1 && p(1) >= 0.2 && (p(0)+p(1) <=1 ) && p(2)<=1 && p(2)>=0),
     domain = (p: Vector[Double]) => p(0) + p(1) <= 1 && p.forall(_ >= 0)
   )
 
   val (viabilityDomain, steps) = approximate(vk, rng)
   println("fin calcul noyau ")
   val output = s"/tmp/BilingualResult2017/"
-  saveVTK3D(viabilityDomain, s"${output}Bilingual${vk.depth}viabdil${vk.dilation}TEST.vtk")
-  saveHyperRectangles(vk)(viabilityDomain, s"${output}Bilingual${vk.depth}dil${vk.dilation}TEST.txt")
+  saveVTK3D(viabilityDomain, s"${output}Bilingual${vk.depth}viabdil${vk.dilation}withk.vtk")
+  saveHyperRectangles(vk)(viabilityDomain, s"${output}Bilingual${vk.depth}dil${vk.dilation}withk.txt")
   val file = new File(s"${output}Bilingual0_5${vk.depth}dil${vk.dilation}Noyau.bin")
 
 //  save(viabilityDomain, file)
