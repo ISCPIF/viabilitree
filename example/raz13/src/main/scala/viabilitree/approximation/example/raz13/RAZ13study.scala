031fea8dea4483177df191273848062cecc806ad
scala <isabelle.alvarez@lip6.fr>
2017-10-02 16:37:19 +0200

    raz13 experimentation kernel

--- example/raz13/src/main/scala/viabilitree/approximation/example/raz13/RAZ13study.scala
+++ example/raz13/src/main/scala/viabilitree/approximation/example/raz13/RAZ13study.scala
@@ -10,12 +10,12 @@ object RAZ13study extends App {
   val riverfront = RAZ13()
   val rng = new util.Random(42)
   val U: Double = 10.0
-  val v: Double = 2.0
-  val depth: Int = 12
+  //  val v: Double = 1.5
+  val depth: Int = 20
 
   val output = s"/tmp/RAZ13Study/"
 
-   def kernel0 = {
+  def kernel0 = {
     import viabilitree.viability._
     import viabilitree.viability.kernel._
 
@@ -28,41 +28,40 @@ object RAZ13study extends App {
       neutralBoundary = Vector(ZoneSide(0, Low), ZoneSide(0, High), ZoneSide(1, High)))
 
     val (ak, steps) = approximate(vk, rng)
-     saveVTK2D(ak, s"${output}raz13${vk.depth}U${U}K0.vtk")
-     saveHyperRectangles(vk)(ak, s"${output}raz13${vk.depth}U${U}K0.txt")
+    saveVTK2D(ak, s"${output}raz13${vk.depth}U${U}K0.vtk")
+    saveHyperRectangles(vk)(ak, s"${output}raz13${vk.depth}U${U}K0.txt")
 
-     (vk, ak, steps)
+    (vk, ak, steps)
   }
+
   //calcule le noyau initial : normalement tout l'espace
   val (vk0, ak0, steps) = kernel0
   println(steps)
 
   //On applique l'inondation de taille v. C'est à dire que les états state se retrouve en (state + perturbation) et on apprend le nouvel ensemble.
-  def thetaV(v:Double,ak:viabilitree.kdtree.Tree[viabilitree.viability.kernel.Content],vk:viabilitree.viability.kernel.KernelComputation) = {
+  def thetaV(v: Double, ak: viabilitree.kdtree.Tree[viabilitree.viability.kernel.Content], vk: viabilitree.viability.kernel.KernelComputation) = {
     import viabilitree.approximation._
 
     val o1 = OracleApproximation(
       depth = depth,
       box = vk0.zone,
-      oracle = (p: Vector[Double]) => riverfront.softJump(p, q => riverfront.jump(q,v) ,ak,vk))
+      oracle = (p: Vector[Double]) => riverfront.softJump(p, q => riverfront.jump(q, v), ak, vk))
 
     val kd1 = approximate(o1)(rng).get
     /*
     Pas la même signature pour OracleApproximation et KernelComputation
-     */
+
 
     saveVTK2D(kd1, s"${output}raz13${vk.depth}U${U}v${v}ORACLE.vtk")
     saveHyperRectangles(o1)(kd1, s"${output}raz13${vk.depth}U${U}v${v}ORACLE.txt")
-
+*/
     (o1, kd1)
   }
 
-  val (o1, kd1) = thetaV(v,ak0,vk0)
-println("ok ak0 erode de v")
 
-  def kernelTheta(kd:viabilitree.kdtree.Tree[viabilitree.approximation.OracleApproximation.Content],
-                  oa:viabilitree.approximation.OracleApproximation,
-                  lesControls: Vector[Double]=>Vector[Control] = vk0.controls) = {
+  def kernelTheta(v:Double,kd: viabilitree.kdtree.Tree[viabilitree.approximation.OracleApproximation.Content],
+                  oa: viabilitree.approximation.OracleApproximation,
+                  lesControls: Vector[Double] => Vector[Control] = vk0.controls) = {
     import viabilitree.viability._
     import viabilitree.viability.kernel._
 
@@ -72,7 +71,7 @@ println("ok ak0 erode de v")
       zone = oa.box,
       controls = lesControls,
       domain = (p: Vector[Double]) => p(0) <= 1.0 && p(0) >= 0,
-      k = Some((p: Vector[Double]) => kd.contains(viabilitree.approximation.OracleApproximation.Content.label.get,p)),
+      k = Some((p: Vector[Double]) => kd.contains(viabilitree.approximation.OracleApproximation.Content.label.get, p)),
       neutralBoundary = Vector(ZoneSide(0, Low), ZoneSide(0, High), ZoneSide(1, High)))
 
     val (ak, steps) = approximate(vk, rng)
@@ -84,8 +83,16 @@ println("ok ak0 erode de v")
 
   }
 
-  val (vk1, ak1, steps1) = kernelTheta(kd1,o1,vk0.controls)
-  println(steps1)
-  println("kernel de K erode v")
+  for (v <- List(0.2, 0.5, 0.8, 1, 2, 2.5, 3, 3.5)) {
+    println("v")
+    println(v)
 
+    val (o1, kd1) = thetaV(v, ak0, vk0)
+    println("ok ak0 erode de v")
+
+    val (vk1, ak1, steps1) = kernelTheta(v,kd1, o1, vk0.controls)
+    println(steps1)
+    println("kernel de K erode v")
+
+  }
 }
