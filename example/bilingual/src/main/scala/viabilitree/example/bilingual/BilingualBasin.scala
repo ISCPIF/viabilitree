a366412abec1acf3a0206a85e678fdcd8a0fe193
ia <isabelle.alvarez@lip6.fr>
2018-06-05 20:22:39 +0200

    Bilingual readme v1

--- example/bilingual/src/main/scala/viabilitree/example/bilingual/BilingualBasin.scala
+++ example/bilingual/src/main/scala/viabilitree/example/bilingual/BilingualBasin.scala
@@ -17,14 +17,17 @@
 
 package viabilitree.example.bilingual
 
-import viabilitree.export._
 import viabilitree.viability._
 import viabilitree.viability.basin._
+
 
 import scala.Ordering.Implicits._
 import scala.collection.immutable._
 import scala.io._
 import scala.util.Random
+
+import viabilitree.export._
+
 
 object BilingualBasin extends App {
 
@@ -53,6 +56,49 @@ object BilingualBasin extends App {
   //  println(volume(eroded))
 
   saveVTK3D(basin, s"/tmp/bilingual${bc.depth}CONTROL.vtk")
+  //  saveVTK3D(eroded, s"/tmp/bilingual${bc.depth}_eroded.vtk")
+
+}
+
+object BilingualBasinFromKernel extends App {
+  implicit val rng = new Random(42)
+  val society = Bilingual(integrationStep = 0.1, timeStep = 1.0)
+
+  def kernelBilingual ={
+    import viabilitree.viability.kernel._
+
+    val vk = KernelComputation(
+      dynamic = society.dynamic,
+      depth = 15,
+      zone = Vector((0.2, 1.0), (0.2, 1.0), (0.0, 1.0)),
+      controls = Vector((0.1 to -0.1 by -0.01)),
+      k = Some(p => p(0) <= 1 && p(0) >= 0.2 && p(1) <= 1 && p(1) >= 0.2),
+      domain = (p: Vector[Double]) => p(0) + p(1) <= 1 && p.forall(_ >= 0) && p.forall(_ <= 1))
+
+    val (viabilityDomain, steps) = approximate(vk, rng)
+    //  val (viabilityDomain, steps) = approximate(vk, rng, maxNumberOfStep = Some(0))
+    println(s"fin calcul noyau ${steps}")
+    (viabilityDomain,steps)
+  }
+
+  val (viabilityDomain,stepK) = kernelBilingual
+
+  val bc = BasinComputation(
+    zone = Vector((0.0, 1.0), (0.0, 1.0), (0.0, 1.0)),
+    depth = 15,
+    pointInTarget = Vector(70.0 / 99, 24.0 / 99, 1.0 / 99),
+    dynamic = society.dynamic,
+    target = p => viabilityDomain.contains(p),
+    controls = Vector((-0.1 to 0.1 by 0.01)),
+    domain = (p: Vector[Double]) => p(0) + p(1) <= 1 && p.forall(_ >= 0))
+
+  val (basin, step) = bc.approximate()
+
+  //  val eroded = erode(bc, basin, rng)
+  println(s"steps $step; volume ${volume(basin)}")
+  //  println(volume(eroded))
+
+  saveVTK3D(basin, s"/tmp/bilingual${bc.depth}FromKernel.vtk")
   //  saveVTK3D(eroded, s"/tmp/bilingual${bc.depth}_eroded.vtk")
 
 }
